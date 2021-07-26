cfb5ebc0eb20f5ce2ef077014cee9b5fa399088e
seba12tg <87844184+seba12tg@users.noreply.github.com>
2021-07-26 00:51:27 -0400

    Update adminregistraguarda.php
    ahora en el formulario no se pueden enviar datos vacios

--- adminregistraguarda.php
+++ adminregistraguarda.php
@@ -8,31 +8,31 @@
 			<tr><td colspan="2">   Registro  de nuevos Guardaparques</td></tr>
 						            <tr>
 				<td> usuario</td>
-				<td><label> <input type="text" placeholder="ingrese nuevo usuario" name="usuario" size="20"/> </label> </td>
+				<td><label> <input type="text" required  placeholder="ingrese nuevo usuario" name="usuario" size="20"/> </label> </td>
             </tr>
 			<tr>
 				<td> Contraseña </td>
-				<td> <label> <input type="text" placeholder="********" name="contrasena" size="20" > </label> </td>
+				<td> <label> <input type="text" required  placeholder="********" name="contrasena" size="20" > </label> </td>
 			</tr>
 			<tr>
 				<td> Rut </td>
-				<td> <label> <input type="text" placeholder="ingrese RUT" name="rut" size="20" > </label> </td>
+				<td> <label> <input type="text" required  placeholder="ingrese RUT" name="rut" size="20" > </label> </td>
 			</tr>
 			<tr>
 				<td> Nombre				</td>
-				<td> <label> <input type="text" placeholder="ingrese nombre" name="nombre" size="20" > </label> </td>
+				<td> <label> <input type="text" required  placeholder="ingrese nombre" name="nombre" size="20" > </label> </td>
 			</tr>
 			<tr>
 			   <td> Apellido paterno </td>
-			   <td> <label> <input type="text" placeholder="ingrese apellido paterno" name="apaterno" size="20"> </label> </td>
+			   <td> <label> <input type="text" required  placeholder="ingrese apellido paterno" name="apaterno" size="20"> </label> </td>
 			</tr>
             <tr>
 				<td>Apellido materno </td>
-				<td> <label><input type="text" placeholder="ingres apellido materno" name="amaterno" size="20"/> </label> </td>
+				<td> <label><input type="text" required  placeholder="ingres apellido materno" name="amaterno" size="20"/> </label> </td>
 			</tr>
             <tr>
 				<td>Telefono</td>
-				<td><label> <input type="text" placeholder="ej.+56978423042" name="telefono" size="20"/> </label> </td>
+				<td><label> <input type="text" required  placeholder="ej.+56978423042" name="telefono" size="20"/> </label> </td>
             </tr>
 			<tr>
 			<td class="boton">
