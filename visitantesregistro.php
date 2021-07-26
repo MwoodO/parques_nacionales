6e5725081724f8ce71490f6b61bb08c5a59ddda1
seba12tg <87844184+seba12tg@users.noreply.github.com>
2021-07-26 00:55:57 -0400

    Update visitantesregistro.php
    este también necesita que estén todos los campos llenos para poder mandar la información a la base de datos

--- visitantesregistro.php
+++ visitantesregistro.php
@@ -9,31 +9,31 @@
 			<tr><td colspan="2">  Visitantes</td></tr>
 						            <tr>
 				<td> Correo</td>
-				<td><label> <input type="text" placeholder="ingrese un correo" name="correo" size="30"/> </label> </td>
+				<td><label> <input type="text" required  placeholder="ingrese un correo" name="correo" size="30"/> </label> </td>
             </tr>
 			<tr>
 				<td> Contraseña </td>
-				<td> <label> <input type="text" placeholder="********" name="contrasena" size="30" > </label> </td>
+				<td> <label> <input type="text" required  placeholder="********" name="contrasena" size="30" > </label> </td>
 			</tr>
 			<tr>
 				<td> Rut </td>
-				<td> <label> <input type="text" placeholder="ej:19292880-k, sin puntos y con guion" name="rut" size="30" > </label> </td>
+				<td> <label> <input type="text" required  placeholder="ej:19292880-k, sin puntos y con guion" name="rut" size="30" > </label> </td>
 			</tr>
 			<tr>
 				<td> Nombre				</td>
-				<td> <label> <input type="text" placeholder="ingrese su nombre" name="nombre" size="30" > </label> </td>
+				<td> <label> <input type="text" required  placeholder="ingrese su nombre" name="nombre" size="30" > </label> </td>
 			</tr>
 			<tr>
 			   <td> Apellido paterno </td>
-			   <td> <label> <input type="text" placeholder="ingrese su apellido paterno" name="apaterno" size="30"> </label> </td>
+			   <td> <label> <input type="text" required  placeholder="ingrese su apellido paterno" name="apaterno" size="30"> </label> </td>
 			</tr>
             <tr>
 				<td>Apellido materno </td>
-				<td> <label><input type="text" placeholder="ingres su apellido materno" name="amaterno" size="30"/> </label> </td>
+				<td> <label><input type="text" required  placeholder="ingres su apellido materno" name="amaterno" size="30"/> </label> </td>
 			</tr>
             <tr>
 				<td>Telefono</td>
-				<td><label> <input type="text" placeholder="ej.+56978423042" name="telefono" size="30"/> </label> </td>
+				<td><label> <input type="text" required  placeholder="ej.+56978423042" name="telefono" size="30"/> </label> </td>
             </tr>
 			<tr>
 			<td class="boton">
