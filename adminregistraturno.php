fada714013a9ecc1cbac432cdd6851824bb68166
seba12tg <87844184+seba12tg@users.noreply.github.com>
2021-07-26 23:20:36 -0400

    Update adminregistraturno.php
    este funciona mejor

--- adminregistraturno.php
+++ adminregistraturno.php
@@ -1,22 +1,43 @@
-<!DOCTYPEhtml>
 <html>
 <head>
 	<title> Turnos	</title>
 </head>
 <body>
-<form name="f1" action="registroturno.php" method="POST">
+<form name="f1" action="adminvalidaturno.php" method="POST">
 <table class="tabla1" width="55%" align="center" cellpadding="0" cellspacing="0">
 			<tr><td colspan="2">  Distribucion de los turnos </td></tr>
 			<tr>
 
 				<td>usuario del guardaparque </td>
-				<td> <label>
-
-	<input 	type="text" name="guardaparque" size="15" > </label> </td>
+				<td> <label>				
+			         <?php
+                      $conexion=mysqli_connect("localhost","root","","parquesnacionales");
+                      $sql="SELECT usuario from guardaparques";
+                      $salida=mysqli_query($conexion,$sql);
+                      $seleccion='<select name="usuariob">';
+                       while($fila=mysqli_fetch_assoc($salida)){
+	                 $seleccion=$seleccion.'<option value="'.$fila['usuario'].'">'.$fila['usuario'].'</option>';
+                          }
+                   $seleccion1=$seleccion.'</select>';
+                    ?>
+					<?php echo $seleccion1; ?>
+	          </label> </td>
 			</tr>
 			<tr>
 				<td> Codigo del parque </td>
-				<td> <label> <input type="text" name="parque" size="15" > </label> </td>
+				<td> <label>	
+    			<?php
+                      $conexion=mysqli_connect("localhost","root","","parquesnacionales");
+                      $sql="SELECT idparque from parquess";
+                      $salida=mysqli_query($conexion,$sql);
+                      $seleccion='<select name="idparqueb">';
+                       while($fila=mysqli_fetch_assoc($salida)){
+	                 $seleccion=$seleccion.'<option value="'.$fila['idparque'].'">'.$fila['idparque'].'</option>';
+                          }
+                   $seleccion=$seleccion.'</select>';
+                    ?>
+					<?php echo $seleccion; ?>
+				 </label> </td>
 			</tr>
 			<tr>
 			   <td> Fecha </td>
@@ -30,7 +51,6 @@
 				<td>Hora de termino</td>
 				<td><label> <input type="time" name="horafinal" size="15"/> </label> </td>
             </tr>
-
 			<tr>
 			<td class="boton">
 			<label>
