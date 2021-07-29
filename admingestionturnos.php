7b10161c9e04096d105bffab26d5259dbd443f42
seba12tg <87844184+seba12tg@users.noreply.github.com>
2021-07-29 01:27:19 -0400

    Add files via upload
    faltaba agregarlo, incluye css

---
+++ admingestionturnos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Administrador</title>
+    <link rel="stylesheet" href="estilo/estilopag_in.css">
+</head>
+<body>
+   
+   <header>
+       <h1>Bienvenido Administrador</h1>
+       
+   </header>
+   <div class="topnav">
+   <ul>
+     <a href="inicio%20paginas.html"> Cerrar sesion </a>
+	 <a href="http://localhost/parquesnacionales/adminpagina.php"> ATRAS </a>
+     <a href="http://localhost/parquesnacionales/adminregistraturno.php"> Registra turnos </a>
+     <a href="http://localhost/parquesnacionales/admin.actualiza.turno.php"> Modifica turnos </a>
+     <a href="http://localhost/parquesnacionales/admineliminaturno.php"> Elimina turnos </a>
+     <a href="http://localhost/parquesnacionales/adminverturnosguardia.php"> Ver turnos </a>
+     </ul>
+</div>
+   <div class="main">      
+        <center><img src="estilo/pngwing.com.png" class="baner" height="300px" width="auto"></center>
+    </div>
+    
+</body>
+</html>
