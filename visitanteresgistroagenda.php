9d6158b4b5a07f0a794541f2b5c79f5004daa573
seba12tg <87844184+seba12tg@users.noreply.github.com>
2021-07-29 02:02:20 -0400

    Update visitanteresgistroagenda.php
    se le agrego vinculo al css

--- visitanteresgistroagenda.php
+++ visitanteresgistroagenda.php
@@ -1,32 +1,40 @@
 <html>
 <head>
-	<title> visitante	</title>
+	<meta charset="utf-8">
+	<title>Reserve su visita</title>
+	<link rel="stylesheet" href="estilo/estiloregistro.css">
 </head>
 <body>
+   <div class="topnav">
+   <ul>
+     <a href="inicio%20paginas.html"> Cerrar sesion </a>
+	 <a href="http://localhost/parquesnacionales/visitantepagina.php">ATRAS</a>
+     </ul>
+</div>
 <form name="f1" action="visitantevalidacionagenda.php" method="POST">
 <table class="tabla1" width="55%" align="center" cellpadding="0" cellspacing="0">
-			<tr><td colspan="2">  Visitantes</td></tr>
+			<tr><th colspan="2"> Visitantes</th></tr>
 						            <tr>
 				<td> Correo</td>
-				<td><label> <input type="text" placeholder="ingrese su correo" name="correo" size="30"/> </label> </td>
+				<td><input type="text" placeholder="ingrese su correo" name="correo" size="30"/></td>
             </tr>
 			<tr>
 				<td> parque</td>
-				<td> <label> <input type="text" placeholder="ingrese el parque que desea visitar" name="parque" size="30" > </label> </td>
+				<td><input type="text" placeholder="ingrese el parque que desea visitar" name="parque" size="30" ></td>
 			</tr>
 			<tr>
 				<td> fecha </td>
-				<td> <label> <input type="date"  name="fecha" size="30" > </label> </td>
+				<td><input type="date"  name="fecha" size="30" > </td>
 			</tr>
 			<tr>
 				<td> acompañantes			</td>
-				<td> <label> <input type="text" placeholder="ingrese cantidad de acompañantes" name="acompanantes" size="30" > </label> </td>
+				<td><input type="text" placeholder="ingrese cantidad de acompañantes" name="acompanantes" size="30" ></td>
 			</tr>
 			<tr>
-			<td class="boton">
-			<label>
+			<td colspan="2" class="boton">
+			
 			 <input type="submit" name="registro" value="registrar">
-			 </label></td>
+			 </td>
 			 </tr>
 </table>
 </form>
