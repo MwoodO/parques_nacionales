90e9687d413975482a5dd7f3597b1dc1cb2d647e
seba12tg <87844184+seba12tg@users.noreply.github.com>
2021-07-29 01:28:27 -0400

    Update adminpagina.php
    se le agrego css y algunos cambios

--- adminpagina.php
+++ adminpagina.php
@@ -1,19 +1,42 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Administrador</title>
+    <meta charset="UTF-8">
+    <title>Administrador</title>
+    <link rel="stylesheet" href="estilo/estilopag_in.css">
 </head>
 <body>
-<div>
-<h1> <center>Bienvenido</center></h1>
-<p>  <center>¿Que desea hacer?</center> </p>
-<table class="tabla" width="400"> 
-<tr>
-	<td width="200"><a href="http://localhost/parquesnacionales/gestionturnos.php" TARGET="_self"> <p class="barra" > gestionar turnos de los guardaparques </p> </a></td>
-</tr>	
-<tr>
-	<td width="200"><a href="http://localhost/parquesnacionales/adminregistraguarda.php" TARGET="_self"><p class="barra" > ingrese nuevos guardaparques </p> </a></td>
-</tr>
-</table>
+   
+   <header>
+       <h1> Bienvenido Administrador </h1>
+       
+   </header>
+   <div class="topnav">
+   <ul>
+     <a href="inicio%20paginas.html">Cerrar sesion</a>
+     <a href="http://localhost/parquesnacionales/adminVerindicevisitante.php">REGISTRO VISITANTES</a>
+     </ul>
 </div>
+       
+   <div>
+   <div class="main">    
+   </div>
+   
+   <article>
+     <div>
+   <div class="main">    
+   
+     <section>
+         <h3> Gestion de turnos Guardaparques</h3>
+         <h3><a class="button" href="http://localhost/parquesnacionales/admingestionturnos.php" > Gestion de turnos </a></h3>
+         
+     </section>  
+     <section>
+         <h3> Gestion de Guardaparques</h3>
+         <h3><a class="button"  href="http://localhost/parquesnacionales/admingestionguarda.php">Gestion Guardaparques</a></h3>
+         
+     </section>
+    </div>
+    
 </body>
 </html>
