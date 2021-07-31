882c88f0244187e3b3e97497ff9fc67da5809390
MwoodO <87843676+MwoodO@users.noreply.github.com>
2021-07-30 22:35:37 -0400

    Update guardavalidaingreso.php

--- PHP/guardaparque/guardavalidaingreso.php
+++ PHP/guardaparque/guardavalidaingreso.php
@@ -6,9 +6,8 @@ $conexion=mysqli_connect('localhost','root','','parquesnacionales');
 if(isset($_POST['registro'])){
 $a = $_POST['codigoagenda'];
 $b = $_POST['acompanantes'];
-$c = $_POST['fecha'];
 $d = $_POST['horaentrada'];
-  $sql="INSERT INTO guardavisita VALUES('".$a."','".$b."','".$c."', '".$d."')";
+  $sql="INSERT INTO guardavisita VALUES('".$a."','".$b."','".$d."')";
 /*ejecutar la consulta*/
 $salida=mysqli_query($conexion,$sql);
 /*validar accion de insercion*/
